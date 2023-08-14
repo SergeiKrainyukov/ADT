8d53c510b7648075b662f956dda6cf3e49ddcaa7
Sergei <sk_developer@mail.ru>
2023-08-14 17:29:58 +0300

    BoundedStack upd

--- src/BoundedStack.java
+++ src/BoundedStack.java
@@ -1,34 +1,74 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class BoundedStack<T> {
+public abstract class BoundedStack<T> {
+
+    public static final int POP_NIL = 0; // push() ещё не вызывалась
+    public static final int POP_OK = 1; // последняя pop() отработала нормально
+    public static final int POP_ERR = 2; // стек пуст
+
+    public static final int PEEK_NIL = 0; // push() ещё не вызывалась
+    public static final int PEEK_OK = 1; // последняя peek() вернула корректное значение
+    public static final int PEEK_ERR = 2; // стек пуст
+
+    public static final int PUSH_OK = 1; // последняя push() отработала нормально
+    public static final int PUSH_ERROR = 2; // в стеке нет свободного места
+
+    // постусловие: создан новый пустой стек
+
+    public BoundedStack(int maxSize) {
+    }
+
+
+    /// команды:
+
+    // предусловие: стек заполнен менее максимально возможного заполнения
+    // постусловие: в стек добавлено новое значение
+    public abstract void push(T value);
+
+    // предусловие: стек не пустой;
+    // постусловие: из стека удалён верхний элемент
+    public abstract void pop();
+
+    // постусловие: из стека удалятся все значения
+    public abstract void clear();
+
+    /// запросы:
+
+    // предусловие: стек не пустой
+    public abstract T peek();
+
+    public abstract int size();
+
+    /// запросы статусов:
+
+    public abstract int get_pop_status();
+
+    public abstract int get_peek_status();
+
+    public abstract int get_push_status();
+
+}
+
+class BoundedStackImpl<T> extends BoundedStack<T> {
 
     private List<T> stack; // основное хранилище стека
     private int peek_status; // статус запроса peek()
     private int pop_status; // статус команды pop()
     private int push_status; // статус команды push()
 
-    private static final int POP_NIL = 0;
-    private static final int POP_OK = 1;
-    private static final int POP_ERR = 2;
-
-    private static final int PEEK_NIL = 0;
-    private static final int PEEK_OK = 1;
-    private static final int PEEK_ERR = 2;
-
-    private static final int PUSH_NIL = 0;
-    private static final int PUSH_OK = 1;
-    private static final int PUSH_ERROR = 2;
-
     private static final int DEFAULT_MAX_SIZE = 32;
 
     private int maxSize = DEFAULT_MAX_SIZE;
 
-    public BoundedStack() {
+    // постусловие: создан новый пустой стек
+    public BoundedStackImpl() {
+        super(DEFAULT_MAX_SIZE);
         clear();
     }
 
-    public BoundedStack(int maxSize) {
+    public BoundedStackImpl(int maxSize) {
+        super(maxSize);
         this.maxSize = maxSize;
         clear();
     }
@@ -64,7 +104,6 @@ public class BoundedStack<T> {
         // начальные статусы для предусловий peek() и pop()
         peek_status = PEEK_NIL;
         pop_status = POP_NIL;
-        push_status = PUSH_NIL;
     }
 
     /// запросы:
